67ec32c45a95bd06e8a6a0da914f5b73e4533f76
Samuel Franssens <u0043016@kuleuven.be>
2018-01-12 14:04:25 +0100

    update read qualtrics

--- R/read_qualtrics.R
+++ R/read_qualtrics.R
@@ -8,8 +8,13 @@
 #' read_qualtrics("data.csv")
 
 read_qualtrics <- function(x){
+  csv1 <- read.csv(x, header = TRUE)
+  csv2 <- read.csv(x, header = TRUE, skip = 1)
+  csv2 <- csv2[-1,]
+  names(csv2) <- names(csv1)
+
   require(dplyr)
-  csv <- dplyr::select(x , -IPAddress,-Progress,-RecordedDate,-ResponseId,-RecipientLastName,-RecipientFirstName,-RecipientEmail,-ExternalReference,-DistributionChannel,-UserLanguage)
+  csv <- dplyr::select(csv2 , -IPAddress,-Progress,-RecordedDate,-ResponseId,-RecipientLastName,-RecipientFirstName,-RecipientEmail,-ExternalReference,-DistributionChannel,-UserLanguage)
   setnames(csv, old = c("StartDate","EndDate","Status","Duration..in.seconds.","Finished","LocationLatitude","LocationLongitude"),
            new = c("start","end","status","duration","finished","latitude","longitude"))
   csv <- as_tibble(csv)
