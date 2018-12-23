96001de126a1a5882b113874e8e615afeb09b1be
Samuel Franssens <u0043016@kuleuven.be>
2018-12-23 18:39:28 +0100

    update remove duplicates

--- R/remove_duplicates.R
+++ R/remove_duplicates.R
@@ -1,12 +1,12 @@
 #' A function that removes duplicates from a dataset
 #'
-#' 
+#'
 #' It requires the following columns: dataset$experiment (optional), dataset$subject, dataset$MID
-#' 
+#'
 #' data$experiment =1 for the experiment that happened first chronologically, = 2 for the experiment that happened second, ...
-#' 
+#'
 #' dataset$subject = 1 for the subject that participated first chronologically, = 2 for the subject that participated second, ...
-#' 
+#'
 #' dataset$MID refers to mturk (or other) IDs
 #' @param dataset A dataframe with experiment (optional), subject, MID as variables.
 #' @keywords duplicates
@@ -15,19 +15,21 @@
 #' dataset <- remove_duplicates(dataset)
 
 remove_duplicates <- function(dataset){
+  library(tidyverse)
+
   if ("experiment" %in% names(dataset)){
-    dataset <- dataset[order(dataset$experiment,dataset$subject),]
+    # dataset <- dataset[order(dataset$experiment,dataset$subject),]
+    dataset <- dataset %>% arrange(experiment, subject) %>% mutate(duplicate = 0)
   } else {
-    dataset <- dataset[order(dataset$subject),]
+    dataset <- dataset %>% arrange(subject) %>% mutate(duplicate = 0)
   }
-  
-  dataset$duplicate <- 0
-  for (i in 2:length(dataset$subject)){
+
+  for (i in 2:nrow(dataset)){
     j <- i-1
     before <- dataset$MID[c(1:j)]
     if (dataset$MID[i] %in% before){
       dataset$duplicate[i] <- 1
-    } 
-  }  
+    }
+  }
   return(dataset)
 }
