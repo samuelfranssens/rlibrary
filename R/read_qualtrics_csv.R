46db4bc0f4b40c705b054c65262d4b9b5441b3f5
Samuel FRANSSENS <samuel.franssens@esc-rennes.com>
2020-05-08 15:51:18 +0200

    small adjustment

--- R/read_qualtrics_csv.R
+++ R/read_qualtrics_csv.R
@@ -9,7 +9,7 @@
 read_qualtrics_csv <- function(filename){
   variable_names <- read_csv(filename) %>% names()
 
-  s22_prolific <- read_csv(filename, skip = 3, col_names = FALSE) %>%
+  read_csv(filename, skip = 3, col_names = FALSE) %>%
     magrittr::set_names(variable_names) %>%
     select(-c("IPAddress", "Progress", "RecordedDate", "ResponseId", "RecipientLastName", "RecipientFirstName", "RecipientEmail", "ExternalReference", "DistributionChannel", "UserLanguage")) %>%
     rename(duration = `Duration (in seconds)`)
