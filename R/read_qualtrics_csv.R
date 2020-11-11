67b109d7cbc030e70a846cb7f2d285f20d48a548
Samuel FRANSSENS <samuel.franssens@rennes-sb.com>
2020-11-11 11:29:34 +0100

    some small changes

--- R/read_qualtrics_csv.R
+++ R/read_qualtrics_csv.R
@@ -7,10 +7,14 @@
 #' read_qualtrics_csv("dataset.csv")
 
 read_qualtrics_csv <- function(filename){
-  variable_names <- read_csv(filename) %>% names()
+
+  variable_names <- read_csv(filename) %>%
+    names() %>%
+    tolower() %>%
+    gsub(" ","_",.) %>%
+    gsub("duration_\\(in_seconds\\)", "duration", .)
 
   read_csv(filename, skip = 3, col_names = FALSE) %>%
     magrittr::set_names(variable_names) %>%
-    select(-c("IPAddress", "Progress", "RecordedDate", "ResponseId", "RecipientLastName", "RecipientFirstName", "RecipientEmail", "ExternalReference", "DistributionChannel", "UserLanguage")) %>%
-    rename(duration = `Duration (in seconds)`)
+    select(-tolower(c("IPAddress", "Progress", "RecordedDate", "ResponseId", "RecipientLastName", "RecipientFirstName", "RecipientEmail", "ExternalReference", "DistributionChannel", "UserLanguage")))
 }
--- R/taskmaster.R
+++ R/taskmaster.R
@@ -8,43 +8,43 @@
 
 taskmaster <- function(df){
 
-  taskmaster_columns <- c("worktimeArray", "tasktime", "offTask", "onTask", "totalOffTask", "totalOnTask", "perPagePT", "pageNo")
+  taskmaster_columns <- c("worktimearray", "tasktime", "offtask", "ontask", "totalofftask", "totalontask", "perpagept", "pageno")
 
   my_data <-  df %>%
     select(all_of(taskmaster_columns))
 
-  totalPages <- max(str_count(my_data$perPagePT, "PAGE BREAK"))+1 # this calculates the number of pages
+  totalpages <- max(str_count(my_data$perpagept, "page break"))+1 # this calculates the number of pages
 
   ## separate arguments:
   # 1. column to split
   # 2. labels of column (finds total columns based on number of pages in survey)
   # 3. Delimiter
 
-  arrayColumnNames <- sprintf("Page_%d", 1:(totalPages)) # into
+  arrayColumnNames <- sprintf("page_%d", 1:(totalpages)) # into
 
-  timingArrays <- my_data %>% # this splits perPagePT into different pages
-    separate(perPagePT, into = arrayColumnNames, sep = "PAGE BREAK", extra = "drop", fill = "right") %>%
+  timingArrays <- my_data %>% # this splits perpagept into different pages
+    separate(perpagept, into = arrayColumnNames, sep = "page break", extra = "drop", fill = "right") %>%
     select(all_of(arrayColumnNames))
 
-  ## Loop through the column for each page and extract additional columns of data (clickAways, timeOffPage, timeOnPage )
-  for(i in seq(totalPages)) {
+  ## Loop through the column for each page and extract additional columns of data (clickaways, timeoffpage, timeonpage )
+  for(i in seq(totalpages)) {
 
     columnName <- names(timingArrays)[i]
     ## Separate so one there is one time per cell
-    boundaryCrossings <- timingArrays %>%
-      separate_(columnName, sprintf("Crossing_%d", 0:max(str_count(timingArrays[,i], "\\."), na.rm = T)), sep = ",", fill = "right") %>%
-      select(-starts_with("Page"), -"Crossing_0") %>%  ## Drop all other columns, ## Drop Crossing_0 column (since each vector starts with a ",")
-      mutate_at(.vars = vars(matches("Crossing")), .funs = as.numeric) ## Convert to numeric
+    boundarycrossings <- timingArrays %>%
+      separate_(columnName, sprintf("crossing_%d", 0:max(str_count(timingArrays[,i], "\\."), na.rm = T)), sep = ",", fill = "right") %>%
+      select(-starts_with("page"), -"crossing_0") %>%  ## Drop all other columns, ## Drop crossing_0 column (since each vector starts with a ",")
+      mutate_at(.vars = vars(matches("crossing")), .funs = as.numeric) ## Convert to numeric
 
     ## Calculate total time on the page
-    timeOnPage <-  apply(boundaryCrossings, 1, function(x) if(all(is.na(x))) {""} else {sum(x[x>0],na.rm = T)}) # if all is NA, then nothing, else, sum of all that is above zero
-    timeOffPage <- apply(boundaryCrossings, 1, function(x) if(all(is.na(x))) {""} else {abs(sum(x[x<0],na.rm = T))})
-    clickAways <-  apply(boundaryCrossings, 1, function(x) if(all(is.na(x))) {""} else {sum(!is.na(x))})
+    timeonpage <-  apply(boundarycrossings, 1, function(x) if(all(is.na(x))) {""} else {sum(x[x>0],na.rm = T)}) # if all is NA, then nothing, else, sum of all that is above zero
+    timeoffpage <- apply(boundarycrossings, 1, function(x) if(all(is.na(x))) {""} else {abs(sum(x[x<0],na.rm = T))})
+    clickaways <-  apply(boundarycrossings, 1, function(x) if(all(is.na(x))) {""} else {sum(!is.na(x))})
 
     # Add to df
     my_data <- my_data %>%
-      bind_cols(tibble(timeOnPage, timeOffPage, clickAways) %>%
-                  set_names(paste0("Page_",i,"_",c("timeOnPage","timeOffPage","ClickAways"))))
+      bind_cols(tibble(timeonpage, timeoffpage, clickaways) %>%
+                  set_names(paste0("page_",i,"_",c("timeonpage","timeoffpage","clickaways"))))
 
   }
 
@@ -53,12 +53,12 @@ taskmaster <- function(df){
     select(-all_of(taskmaster_columns)) %>%
     bind_cols(my_data) %>%
     select(-all_of(taskmaster_columns)) %>%
-    mutate_at(.vars = vars(matches("OffPage")), .funs = as.character) %>%
-    mutate_at(.vars = vars(matches("OnPage")), .funs = as.character) %>%
-    mutate_at(.vars = vars(matches("OffPage")), .funs = as.character) %>%
-    mutate_at(.vars = vars(matches("ClickAways")), .funs = as.character) %>%
-    mutate_at(.vars = vars(matches("OffPage")), .funs = as.numeric) %>%
-    mutate_at(.vars = vars(matches("OnPage")), .funs = as.numeric) %>%
-    mutate_at(.vars = vars(matches("OffPage")), .funs = as.numeric) %>%
-    mutate_at(.vars = vars(matches("ClickAways")), .funs = as.numeric)
+    mutate_at(.vars = vars(matches("offpage")), .funs = as.character) %>%
+    mutate_at(.vars = vars(matches("onpage")), .funs = as.character) %>%
+    mutate_at(.vars = vars(matches("offpage")), .funs = as.character) %>%
+    mutate_at(.vars = vars(matches("clickaways")), .funs = as.character) %>%
+    mutate_at(.vars = vars(matches("offpage")), .funs = as.numeric) %>%
+    mutate_at(.vars = vars(matches("onpage")), .funs = as.numeric) %>%
+    mutate_at(.vars = vars(matches("offpage")), .funs = as.numeric) %>%
+    mutate_at(.vars = vars(matches("clickaways")), .funs = as.numeric)
 }
