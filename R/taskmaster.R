d5895c09b856077e29e23aaee04a1ba5da4fb3a8
Samuel FRANSSENS <samuel.franssens@rennes-sb.com>
2020-11-13 13:52:59 +0100

    bug fix taskmaster

--- R/taskmaster.R
+++ R/taskmaster.R
@@ -9,7 +9,7 @@
 taskmaster <- function(df){
 
   taskmaster_columns <- c("worktimearray", "tasktime", "offtask", "ontask", "totalofftask", "totalontask", "perpagept", "pageno")
-  df <- df %>% drop_na(worktimearray)  # otherwise you get errors
+  df <- df %>% drop_na(perpagept)  # otherwise you get errors
 
   # extract the data we will use
   my_data <- df %>% select(all_of(taskmaster_columns))
@@ -30,22 +30,23 @@ taskmaster <- function(df){
 
   ## Loop through the column for each page and extract additional columns of data (clickAways, timeOffPage, timeOnPage )
   for(i in seq(totalPages)) {
-
+    print(i)
     split_variable <- timingArrays %>% select(all_of(paste0("Page_",i))) %>% set_names("v") # which variable do we need to split
 
     max_number_vars <- max(str_count(split_variable$v, "\\,"))+1
+    if(!is.na(max_number_vars)){
+      results <- split_variable %>%
+        separate(v, into = paste0("Crossing_",0:max_number_vars), sep = ",", fill = "right") %>%
+        select(-Crossing_0) %>%
+        mutate_all(.funs = as.numeric)
 
-    results <- split_variable %>%
-      separate(v, into = paste0("Crossing_",0:max_number_vars), sep = ",", fill = "right") %>%
-      select(-Crossing_0) %>%
-      mutate_all(.funs = as.numeric)
-
-    # Add to df
-    my_data <- my_data %>%
-      bind_cols(tibble(timeOnPage = apply(results,  1, function(x) if(all(is.na(x))) {0} else {sum(x[x>0],na.rm = T)}), # sum of positive values
-                       timeOffPage = apply(results, 1, function(x) if(all(is.na(x))) {0} else {abs(sum(x[x<0],na.rm = T))}), # sum of negative values
-                       clickAways = apply(results,  1, function(x) if(all(is.na(x))) {0} else {sum(!is.na(x))})) %>%  # sum of clicks
-                  set_names(paste0("p",i,"_",c("timeonpage","timeoffpage","clickaways"))))
+      # Add to df
+      my_data <- my_data %>%
+        bind_cols(tibble(timeOnPage = apply(results,  1, function(x) if(all(is.na(x))) {0} else {sum(x[x>0],na.rm = T)}), # sum of positive values
+                         timeOffPage = apply(results, 1, function(x) if(all(is.na(x))) {0} else {abs(sum(x[x<0],na.rm = T))}), # sum of negative values
+                         clickAways = apply(results,  1, function(x) if(all(is.na(x))) {0} else {sum(!is.na(x))})) %>%  # sum of clicks
+                    set_names(paste0("p",i,"_",c("timeonpage","timeoffpage","clickaways"))))
+    }
 
   }
 
